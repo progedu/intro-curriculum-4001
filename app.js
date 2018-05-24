4e9b9ce5100ff881e8bab2c47195ae55885c30b8
DDRAGON <davide.0829@gmail.com>
2018-05-24 14:19:50 +0900

    Express のアップデート by 一括変換ツール

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
+var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
@@ -34,7 +34,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.use('/', index);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
